23b056587fff84633ff7a50bfb1663e9f4f0e07d
Derek Smith <smithda1@masters.edu>
2026-05-22 10:34:18 -0700

    about

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -21,7 +21,7 @@ function About() {
         </p>
 
         <section className="about-section">
-          <h2>Created To Worship</h2>
+          <h2>What is the purpose?</h2>
           <p>
             The title, "Worthy for Worship," describes the heart behind this
             site. We are all created for the purpose of worship. One specific
@@ -31,11 +31,16 @@ function About() {
         </section>
 
         <section className="about-section">
-          <h2>Why This Site?</h2>
+          <h2>Why use this site?</h2>
           <p>
-            I do not mean to take away from the many excellent worship resources
+            I do not mean to discourage use of other excellent worship resources
             that already exist. However, many are built for a broad category of
-            people from different churches with different needs.
+            people who come from different churches with different needs.
+            My site is spicifically tailored for personal use in the local church. 
+            This is helpful because you can share specific arrangements that your church
+            is used to or even refer to other peoples song lists to know what songs are 
+            acceptable to play on a Sunday morning. 
+            
           </p>
           <p>
             Worthy for Worship is focused on the local church and individual
@@ -45,18 +50,53 @@ function About() {
           <p>
             I am building this site originally for my own church and team. 
             Nevertheless I hope it can be an encouragment to others in the 
-            universal church as well!
+            universal church as well as I continue to develop and expand. 
+          </p>
+          <p>
+            All glory to Christ, <br></br>
+            Derek Smith
           </p>
         </section>
-
-        <section className="about-section about-closing">
-          <h2>Built For Sharing</h2>
+        <section className="about-section">
+          <h2>How to use?</h2>
           <p>
-            Each account is free and comes with a personal library of songs that
-            can only be shared through friend requests. It is up to you to
-            create faithful, helpful charts, but I pray this site can make those
-            charts easier to share with your team as you grow in serving the
-            church through music.
+            Each account is free and comes with a section for a personal library of songs that
+            can only be shared through friend requests. This is intentional because this site is 
+            not meant to be free distribution copywrited charts. 
+          </p>
+          <p>
+            It is up to you to create faithful, helpful charts to share with your church!
+          </p>
+          <p>
+            There will also be development on other helpful resouces sourounding worship 
+            including tutorials and articles for your free use! 
+          </p>
+        </section>
+         <section className= "about-section">
+          <h2>
+            What are the rules?
+          </h2>
+          <p>
+            Under the CCLI Church Copyright License and CCLI Rehearsal License, a church is only 
+            allowed to distribute digital files (chords, lyrics, sheets) to individuals who are 
+            active members of that specific congregation's worship team. 
+          </p>
+          <p>
+            Under CCLI terms, you may only upload chord sheets, lyrics, or custom lead sheets 
+            created by your ministry. Scanning and uploading retail sheet music books, piano accompaniments,
+            or choral octavos is strictly prohibited.
+          </p>
+          <p>
+            I suggest being very careful in uploading song sheets to make sure you have proper rights to.
+            When creating a chart, there must be credit given at the bottom such as a CCLI #.
+            The only instance this does not matter is if the song is in the public domain. 
+            (all musical compositions published in the United States in or before 1930 are in the public domain)
+          </p>
+          <p>
+            If these rules are not followed properly, I reserve the right to remove your charts from my website.
+          </p>
+          <p>
+            If you have any questions, reach out to me at derek.smith@worthyforworship.com
           </p>
         </section>
       </section>
