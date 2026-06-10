73e98f8810ea0df4ec360571cb76addeba9eceeb
Derek Smith <smithda1@masters.edu>
2026-06-09 19:52:10 -0700

    tutorials

--- src/Pages/Tutorials.js
+++ src/Pages/Tutorials.js
@@ -5,6 +5,19 @@ function Tutorials() {
     <main className="page">
       <h1>Tutorials</h1>
       <p>Walkthroughs and practice guides will live here.</p>
+      <br />
+      <p className="tutorials-contact-copy">
+        If you have any tutorials that you are working on and think might be
+        useful for those practicing for ministry in the church, reach out to me
+        at{" "}
+        <a
+          className="contact-email-link"
+          href="mailto:derek.smith@worthyforworship.com"
+        >
+          derek.smith@worthyforworship.com
+        </a>{" "}
+        and we can talk about putting them here!
+      </p>
     </main>
   );
 }
